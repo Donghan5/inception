f9de69753e3f0e25881950bd3340b9f7706d9868
donghank <donghank@donghank.myguest.virtualbox.org>
2025-01-31 12:33:26 +0100

    find the way

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -7,21 +7,18 @@ else
 	# download wordpress
 	wget "https://wordpress.org/latest.tar.gz"
 	tar -xzvf latest.tar.gz
-	mv wordpress/* .
 	rm -rf latest.tar.gz
-	rm -rf wordpress
 
 	# reset configuratoin file
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ./www.conf /etc/7.3/fpm/pool.d/
+	mv ./www.conf /etc/php/7.3/fpm/pool.d/
 
 	# set the wordpress file
 	cd /var/www/html/wordpress
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 fi
 
 
