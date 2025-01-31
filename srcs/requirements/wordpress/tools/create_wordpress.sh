694126cfe2b96410bb983b3971b6301dbf67e7d8
Donghan Kim <donghank@paul-f4Br3s4.clusters.42paris.fr>
2025-01-31 15:26:36 +0100

    wordpress sh change

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [ -f ./wordpress/wp-config.php ]
-then
-	echo "configure file is already exist"
-else
-	# download wordpress
-	wget "https://wordpress.org/latest.tar.gz"
-	tar -xzvf latest.tar.gz
-	rm -rf latest.tar.gz
+cd /var/www/html
 
-	# reset configuratoin file
-	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ./www.conf /etc/php/7.3/fpm/pool.d/
+if [ ! -f ./wordpress/wp-config.php ]; then
+    echo "Downloading WordPress..."
+    wget -q "https://wordpress.org/latest.tar.gz"
+    tar -xzf latest.tar.gz
+    rm -f latest.tar.gz
 
-	# set the wordpress file
-	cd /var/www/html/wordpress
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+    echo "Creating wp-config.php..."
+    cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
+
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" ./wordpress/wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/g" ./wordpress/wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/g" ./wordpress/wp-config.php
+    sed -i "s/localhost/$MYSQL_HOSTNAME/g" ./wordpress/wp-config.php
+
+    chown www-data:www-data ./wordpress/wp-config.php
+    chmod 644 ./wordpress/wp-config.php
 fi
 
 
